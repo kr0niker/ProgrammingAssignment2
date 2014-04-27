c9f329d0801ebcd669a61fbb52ba70a6f32c82d4
Kroniker <yndx.kroniker@yandex.ru>
2014-04-27 22:55:09 +0200

    programming assignement1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## I am using the function ginv() from the "MASS" package that returns the inversed matrix
+## The functions allows to make a matrix, to get the matrix, to set its inverse and to get its inverse 
+## (analoquosly to the function from the example)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                m <- NULL
+                set <- function(y) {
+                        x <<- y
+                        m <<- NULL
+                }
+                get <- function() x
+                setinv <- function(inv) m <<- inv
+                getinv <- function() m
+                list(set = set, get = get,
+                     setinv = setinv,
+                     getinv = getinv)
+        
 }
 
 
-## Write a short comment describing this function
+## This function checks if the inversed matrix is already set and stored in cache, if so, returns it, if not, computes and returns it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+                m <- x$getinv()
+                if(!is.null(m)) {
+                        message("getting cached data")
+                        return(m)
+                }
+                data <- x$get()
+                m <- ginv(data, ...)
+                x$setinv(m)
+                m
+
 }
